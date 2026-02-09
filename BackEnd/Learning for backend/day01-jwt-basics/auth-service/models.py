b84017d9286e4813cb4227bd2a0ac3669459a276
NikitaAbichkin <abichkinnnikita@gmail.com>
2026-02-09 20:26:40 +0500

    changed new folder

---
+++ BackEnd/Learning for backend/day01-jwt-basics/auth-service/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timezone
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from database import Base
+
+class User (Base):
+    __tablename__ = "users"
+    id:Mapped[int] =  mapped_column(primary_key=True)
+    username:Mapped[str] = mapped_column(String(50),unique=True,index=True)
+    hashed_password: Mapped[str] = mapped_column(String (5000))
+    created_at:Mapped[datetime] = mapped_column(default= lambda:datetime.now(timezone.utc))
+
+
